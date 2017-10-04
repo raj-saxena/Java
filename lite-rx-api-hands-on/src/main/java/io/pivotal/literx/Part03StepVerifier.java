1020005a5f3a2fcefcb1968444ced1917e1a01da
Raj <the.raj.saxena@gmail.com>
2017-10-04 14:17:48 +0200

    Solutions part 03.

--- lite-rx-api-hands-on/src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ lite-rx-api-hands-on/src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -16,10 +16,15 @@
 
 package io.pivotal.literx;
 
-import java.util.function.Supplier;
-
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
+
+import java.util.function.Supplier;
+
+import static java.time.Duration.ofSeconds;
+import static org.assertj.core.api.Assertions.assertThat;
+import static reactor.test.StepVerifier.create;
+import static reactor.test.StepVerifier.withVirtualTime;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
@@ -27,47 +32,58 @@ import reactor.core.publisher.Flux;
  * @author Sebastien Deleuze
  * @see <a href="http://projectreactor.io/docs/test/release/api/reactor/test/StepVerifier.html">StepVerifier Javadoc</a>
  */
-public class Part03StepVerifier {
+class Part03StepVerifier {
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
-	void expectFooBarComplete(Flux<String> flux) {
-		fail();
-	}
+    void expectFooBarComplete(Flux<String> flux) {
+        create(flux)
+                .expectNext("foo", "bar")
+                .verifyComplete();
+    }
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
-	void expectFooBarError(Flux<String> flux) {
-		fail();
-	}
+    // Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
+    void expectFooBarError(Flux<String> flux) {
+		create(flux)
+                .expectNext("foo", "bar")
+                .verifyError(RuntimeException.class);
+    }
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
-	// and another one with "jpinkman" then completes successfully.
-	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
-	}
+    // Use StepVerifier to check that the flux parameter emits a User with "swhite"username
+    // and another one with "jpinkman" then completes successfully.
+    void expectSkylerJesseComplete(Flux<User> flux) {
+        create(flux)
+                .expectNextMatches(user -> user.getUsername().equals("swhite"))
+                .assertNext(user -> assertThat(user.getUsername().equals("jpinkman")))
+                .verifyComplete();
+    }
 
 //========================================================================================
 
-	// TODO Expect 10 elements then complete and notice how long the test takes.
-	void expect10Elements(Flux<Long> flux) {
-		fail();
-	}
+    // Expect 10 elements then complete and notice how long the test takes.
+    void expect10Elements(Flux<Long> flux) {
+        create(flux)
+                .expectNextCount(10)
+                .verifyComplete();
+    }
 
 //========================================================================================
 
-	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
-	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
-	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
-	}
+    // Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
+    // by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
+    void expect3600Elements(Supplier<Flux<Long>> supplier) {
+        withVirtualTime(supplier)
+                .thenAwait(ofSeconds(3600))
+                .expectNextCount(3600)
+                .verifyComplete();
+    }
 
-	private void fail() {
-		throw new AssertionError("workshop not implemented");
-	}
+    private void fail() {
+        throw new AssertionError("workshop not implemented");
+    }
 
 }
